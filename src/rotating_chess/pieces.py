1d89c4d60b8bb05b5fa40ac9986d333241c0c86b
wade-cheng <wadecheng3301@gmail.com>
2025-03-23 12:46:12 -0400

    bugfix

--- src/rotating_chess/pieces.py
+++ src/rotating_chess/pieces.py
@@ -1,6 +1,5 @@
 import os
 import math
-import settings
 import pygame
 from pygame.locals import *
 import itertools
@@ -9,6 +8,7 @@ from enum import Enum
 from itertools import chain
 from collections.abc import Iterable
 
+from rotating_chess import settings as settings
 from rotating_chess.debug import *
 
 
